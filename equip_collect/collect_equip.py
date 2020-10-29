c0e26469d256a549dbc8848869af55bd118b8c1d
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2020-10-29 14:54:54 +0900

    폴더=파이에서장비정보수집

--- equip_collect/collect_equip.py
+++ equip_collect/collect_equip.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import collector_config as cfg
+import ps_config as cfg
 import pymysql
 
 
 def main():
     conn=pymysql.connect(host=cfg.DB_host,user=cfg.DB_user,password=cfg.DB_password,db=cfg.DB_db,charset='utf8')
     curs=conn.cursor()
-    sql='insert into equip_performance(eq_ip,record_date,cpu_use,memory_use,disk_use) values (NULL,%s,%s,%s,%s)'
+    sql='insert into equip_performance(eq_ip,record_date,cpu_use,memory_use,disk_use) values (%s,%s,%s,%s,%s)'
     #curs.execute(sql,(cfg.ip,cfg.nowDatetime,cfg.cpu,cfg.memory,cfg.disk))
-    curs.execute(sql,(cfg.nowDatetime,cfg.cpu,cfg.memory,cfg.disk))
+    curs.execute(sql,(cfg.ip,cfg.nowDatetime,cfg.cpu,cfg.memory,cfg.disk))
 
     sql='select * from equip_performance'
     curs.execute(sql)   
@@ -19,6 +19,12 @@ def main():
     conn.close()
     
     
-        
+    
+    
+    
+    
+    
+    
+    
 if __name__=="__main__":
     main()
--- equip_collect/ps_config.py
+++ equip_collect/ps_config.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import datetime
-import socket
+
 import psutil
+import socket, fcntl, struct
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s','eth0'))[20:24])
+
 
 now=datetime.datetime.now()
 nowtime=now.strftime('%Y-%m-%d')#년-원-일
@@ -10,7 +14,7 @@ nowDatetime=now.strftime('%Y-%m-%d %H:%M:%S')#년-원-일-시-분-초
 cpu=psutil.cpu_percent(interval=None, percpu=False)  #cpu%
 memory=psutil.disk_usage('/').percent                #memory%
 disk=str((psutil.disk_usage('/').free)/(1024.0 ** 3))[0:7]   #disk %
-ip=socket.gethostbyname(socket.gethostname())
+#ip=socket.gethostbyname(socket.gethostname())
 
 # print({cpu},{memory},{disk},{ip})
 
